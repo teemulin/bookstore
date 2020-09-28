514e560686c055a9c7ce5cae345d2c5875887f22
teemulin <63881696+teemulin@users.noreply.github.com>
2020-09-28 13:21:54 +0300

    added spring data REST functionality

--- src/main/java/fi/teemuli/Bookstore/domain/Category.java
+++ src/main/java/fi/teemuli/Bookstore/domain/Category.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Category {
 	
@@ -18,6 +20,7 @@ public class Category {
 	
 	private String name;
 	
+	@JsonBackReference
 	@OneToMany(cascade = CascadeType.ALL, mappedBy ="category")
 	private List<Book> books;
 	
