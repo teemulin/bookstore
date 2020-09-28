cbaf07c18b4c80300a820dbdcc07d5297eab3e1a
teemulin <63881696+teemulin@users.noreply.github.com>
2020-09-28 13:30:07 +0300

    added RESTful search function

--- src/main/java/fi/teemuli/Bookstore/domain/CategoryRepository.java
+++ src/main/java/fi/teemuli/Bookstore/domain/CategoryRepository.java
@@ -3,9 +3,12 @@ package fi.teemuli.Bookstore.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
-	List<Category> findByName(String name);
+	List<Category> findByName(@Param("name") String name);
 	
 }
